30156405c3a92d017fb5969d293795727a0bc0c1
luffytentation5-art <luffytentation5@gmail.com>
2026-03-18 19:58:29 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
 }
-@app.route("/")
-def home():
-    return "WORKER OK"
+
+@app.route("/fetch")
+def fetch():
     url = request.args.get("url")
 
     if not url:
@@ -19,3 +19,14 @@ def home():
         return Response(r.content, content_type=r.headers.get("Content-Type"))
     except:
         return "Error"
+
+
+# 🔥 CAPTURA TODO (ESTO ARREGLA EL 404)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return "Ruta no válida en proxy"
+def rewrite_html(content, base_url):
+    content = content.replace('href="/', f'href="/proxy?url={base_url}/')
+    content = content.replace('src="/', f'src="/proxy?url={base_url}/')
+    return content
